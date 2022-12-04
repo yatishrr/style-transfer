5f1680da0a69f6b5de3f00f5158a4c9da418fe5a
Ubuntu <ubuntu@ip-10-0-16-60.ap-south-1.compute.internal>
2022-12-04 20:24:54 +0000

    added keycloak login

--- frontend/main.py
+++ frontend/main.py
@@ -3,6 +3,7 @@ import time
 import requests
 import streamlit as st
 from PIL import Image
+from streamlit_keycloak import login
 
 STYLES = {
     "candy": "candy",
@@ -16,37 +17,49 @@ STYLES = {
     "udnie": "udnie",
 }
 
-st.set_option("deprecation.showfileUploaderEncoding", False)
 
-st.title("Style transfer web app")
+def main():
+    st.set_option("deprecation.showfileUploaderEncoding", False)
 
-image = st.file_uploader("Choose an image")
+    image = st.file_uploader("Choose an image")
 
-style = st.selectbox("Choose the style", [i for i in STYLES.keys()])
+    style = st.selectbox("Choose the style", [i for i in STYLES.keys()])
 
-if st.button("Style Transfer"):
-    if image is not None and style is not None:
-        files = {"file": image.getvalue()}
-        res = requests.post(f"http://backend:8080/{style}", files=files)
-        img_path = res.json()
-        image = Image.open(img_path.get("name"))
-        st.image(image)
+    if st.button("Style Transfer"):
+        if image is not None and style is not None:
+           files = {"file": image.getvalue()}
+           res = requests.post(f"http://backend:8080/{style}", files=files)
+           img_path = res.json()
+           image = Image.open(img_path.get("name"))
+           st.image(image)
 
-        displayed_styles = [style]
-        displayed = 1
-        total = len(STYLES)
+           displayed_styles = [style]
+           displayed = 1
+           total = len(STYLES)
 
-        st.write("Generating other models...")
+           st.write("Generating other models...")
 
-        while displayed < total:
-            for style in STYLES:
-                if style not in displayed_styles:
-                    try:
-                        path = f"{img_path.get('name').split('.')[0]}_{STYLES[style]}.jpg"
-                        image = Image.open(path)
-                        st.image(image, width=500)
-                        time.sleep(1)
-                        displayed += 1
-                        displayed_styles.append(style)
-                    except:
+           while displayed < total:
+               for style in STYLES:
+                   if style not in displayed_styles:
+                      try:
+                          path = f"{img_path.get('name').split('.')[0]}_{STYLES[style]}.jpg"
+                          image = Image.open(path)
+                          st.image(image, width=500)
+                          time.sleep(1)
+                          displayed += 1
+                          displayed_styles.append(style)
+                      except:
                         pass
+
+
+st.title("style-transfer")
+keycloak = login(
+    url="https://style-transfer.awsexpress.cloud/auth",
+    realm="master",
+    client_id="style-transfer",
+)
+
+if keycloak.authenticated:
+    time.sleep(5)
+    main()
